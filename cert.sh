bae3747779b131ba62258db834b779893eabe8b2
一灯和尚 <46807619+mrknow001@users.noreply.github.com>
2022-11-30 22:35:30 +0800

    Update cert.sh

--- cert.sh
+++ cert.sh
@@ -2,7 +2,7 @@ sudo apt install certbot -y
 echo "*******tips*******"
 echo "1.请按提示添加域名的txt记录 "
 echo "2.申请成功后会生成fullchain.pem和privkey.pem文件 "
-#certbot certonly --manual --preferred-challenges=dns --email test@web.de --server https://acme-v02.api.letsencrypt.org/directory --agree-tos -d \*.$1 -d $1
+certbot certonly --manual --preferred-challenges=dns --email test@web.de --server https://acme-v02.api.letsencrypt.org/directory --agree-tos -d \*.$1 -d $1
 cp /etc/letsencrypt/archive/$1/fullchain1.pem ./fullchain.pem
 cp /etc/letsencrypt/archive/$1/privkey1.pem ./privkey.pem
 
